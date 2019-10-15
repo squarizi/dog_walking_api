5250ececef4c3c78c071d9f7adb9ffd1bf56bf83
squarizi <fesqua@yahoo.com.br>
2019-10-15 11:00:04 -0300

    Create DogWalking model

---
+++ app/models/dog_walking.rb
@@ -0,0 +1,38 @@
+class DogWalking < ActiveRecord::Base
+
+  enum status: { scheduled: 0, on_going: 1, finished: 2 }
+
+  enum duration: { half_hour: 30, hour: 60 }
+
+  validates :scheduled_at, :duration, :latitude, :longitude, :pets_quantity, presence: true
+
+  scope :today, -> { where(scheduled_at: Time.now..Time.now.end_of_day) }
+
+  def start_walking!
+    return false unless scheduled?
+
+    self.status = :on_going
+    self.start_walk = Time.now
+    save!
+  end
+
+  def finish_walking!
+    return false unless on_going?
+
+    self.status = :finished
+    self.finish_walk = Time.now
+    save!
+  end
+
+  def time_walking
+    Time.at(time_walking_in_seconds).gmtime.strftime('%R:%S')
+  end
+
+  private
+
+  def time_walking_in_seconds
+    return 0 if scheduled?
+    return Time.now - start_walk if on_going?
+    return finish_walk - start_walk if finished?
+  end
+end
---
+++ spec/models/dog_walking_spec.rb
@@ -0,0 +1,107 @@
+require 'rails_helper'
+
+RSpec.describe DogWalking, type: :model do
+
+  subject {
+            DogWalking.new(scheduled_at: Time.now, price: 30.50,
+                           duration: :half_hour, latitude: 44.4604788,
+                           longitude: -110.8281375, pets_quantity: 4)
+          }
+
+  context '#status enum' do
+    let(:status) { { "scheduled" => 0, "on_going" => 1, "finished" => 2 } }
+
+    it { expect(described_class.statuses).to eq status }
+  end
+
+  context '#duration enum' do
+    let(:duration) { { "half_hour" => 30, "hour" => 60 } }
+
+    it { expect(described_class.durations).to eq duration }
+  end
+
+  describe '#start_walking' do
+    context "when status is scheduled" do
+
+      before { subject.start_walking! }
+
+      it do
+        expect(subject.persisted?).to be_truthy
+        expect(subject.start_walk).to be_present
+        expect(subject.on_going?).to be_truthy
+      end
+    end
+
+    context "when status is on_going" do
+      before { subject.status = :on_going }
+
+      it { expect(subject.start_walking!).to be_falsey }
+    end
+
+    context "when status is finished" do
+      before { subject.status = :finished }
+
+      it { expect(subject.start_walking!).to be_falsey }
+    end
+  end
+
+  describe '#finish_walking' do
+    context "when status is on_going" do
+
+      before do
+        subject.status = :on_going
+        subject.finish_walking!
+      end
+
+      it do
+        expect(subject.persisted?).to be_truthy
+        expect(subject.finish_walk).to be_present
+        expect(subject.finished?).to be_truthy
+      end
+    end
+
+    context "when status is scheduled" do
+      before { subject.status = :scheduled }
+
+      it { expect(subject.finish_walking!).to be_falsey }
+    end
+
+    context "when status is finished" do
+      before { subject.status = :finished }
+
+      it { expect(subject.finish_walking!).to be_falsey }
+    end
+  end
+
+  describe '#time_walking' do
+
+    context 'when status is scheduled' do
+      let(:time) { '00:00:00' }
+
+      it { expect(subject.time_walking).to eq time }
+    end
+
+    context 'when status is on_going' do
+      let(:time) { '00:30:00' }
+
+      before do
+        subject.status = :on_going
+        subject.start_walk = Time.now - 30.minutes
+      end
+
+      it { expect(subject.time_walking).to eq time }
+    end
+
+    context 'when status is finished' do
+      let(:time) { '00:30:00' }
+
+      before do
+        subject.status = :finished
+        subject.start_walk = Time.now
+        subject.finish_walk = subject.start_walk + 30.minutes
+      end
+
+      it { expect(subject.time_walking).to eq time }
+    end
+  end
+end
